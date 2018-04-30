32322772cebe28c98c856623173adcffc44a2c08
GerardoMac <centrodph@gmail.com>
2018-04-30 16:41:46 -0300

    simple test socket connection

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import './assets/App.scss';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
 
 class App extends Component {
+    componentDidMount() {
+        var sock = new SockJS('http://localhost:8080/socket');
+        sock.onopen = function() {
+            console.log('open');
+        };
+        sock.onmessage = function(e) {
+            console.log('message', e.data);
+        };
+        sock.onclose = function() {
+            console.log('close');
+        };
+        const stompClient = Stomp.over(sock);
+        stompClient.connect({}, function(frame) {
+            console.info('Connected: ' + frame);
+            stompClient.subscribe('/topic/greetings', function(greeting) {
+                console.info(greeting);
+            });
+            stompClient.subscribe('/app/topic', function(greeting) {
+                console.info(greeting);
+            });
+        });
+    }
+
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    GP
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <header className="App-header">GP</header>
+                <p className="App-intro">Works</p>
             </div>
         );
     }
